ea6748ab2d73ad30edd58b506b81f8c2b9fd7529
Peter Rindal <perindal@visa.com>
2023-02-19 12:19:45 -0800

    no sys

--- thirdparty/getMacoro.cmake
+++ thirdparty/getMacoro.cmake
@@ -1,6 +1,6 @@
 
 set(GIT_REPOSITORY      "https://github.com/ladnir/macoro.git")
-set(GIT_TAG             "06d05ef619ccf99d0560e97538ce01f07aaaf602" )
+set(GIT_TAG             "649ee0235b4d77f336ec8508a9502484284c4142" )
 
 set(CLONE_DIR "${COPROTO_THIRDPARTY_CLONE_DIR}/macoro")
 set(BUILD_DIR "${CLONE_DIR}/out/build/${COPROTO_CONFIG}")
