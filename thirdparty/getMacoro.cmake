c9e646b2f350c998e5225a67166573ad88ac9dbd
Peter Rindal <peterrindal@gmail.com>
2024-12-05 21:02:36 -0800

    macoro bump

--- thirdparty/getMacoro.cmake
+++ thirdparty/getMacoro.cmake
@@ -4,7 +4,7 @@ set(GIT_REPOSITORY      "https://github.com/ladnir/macoro.git")
 if(DEFINED MACORO_GIT_TAG)
     set(GIT_TAG ${MACORO_GIT_TAG})
 else()
-    set(GIT_TAG "8d27637cac2169f95e3a3456286384bb3d81e5ec" )
+    set(GIT_TAG "305b716ec78e9d65275943bd53fa0ed4a7defee8" )
 endif()
 
 set(CLONE_DIR "${COPROTO_THIRDPARTY_CLONE_DIR}/macoro")
